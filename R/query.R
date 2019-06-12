701d79929a9fe09b667ab9a86637143fdddbec90
Chanyub Park <mrchypark@gmail.com>
2019-06-13 06:51:24 +0900

    chg search to query

--- R/query.R
+++ R/query.R
@@ -1,21 +1,30 @@
-#' search name of peaple who worked for Korea
+#' query name of peaple who work(ed) for Korea Government
 #'
 #' This function use newstapa site \url{https://jaesan.newstapa.org/}
 #'
-#' @param name_query query string for search name.
+#' @param name query string for search name or title in gov.
+#' @param msg announce root url. Defualt is TRUE.
+#'
 #' @importFrom httr add_headers POST content
 #' @importFrom rvest html_nodes html_text
 #' @importFrom jsonlite fromJSON
 #' @importFrom dplyr as_tibble
+#' @importFrom attempt stop_if_not
+#'
 #' @return a [tibble][tibble::tibble-package]
+#'
 #' @export
-search <- function(name_query) {
-  stop_if_not(.x = curl::has_internet(),
+
+query <- function(name, msg = T) {
+  attempt::stop_if_not(.x = curl::has_internet(),
               msg = "You should have internet to do that")
-  message('root url is "https://jaesan.newstapa.org/"')
+  if (msg) {
+    message('root url is "https://jaesan.newstapa.org/"')
+  }
   httr::POST("https://jaesan.newstapa.org/get_list",
-             httr::add_headers(`X-Requested-With` = "XMLHttpRequest"),
-             body = list(dataType = "json", phrase = name_query)) %>%
+             config = httr::add_headers(`X-Requested-With` = "XMLHttpRequest"),
+             body = list(dataType = "json", phrase = name),
+             encode = "json") %>%
     httr::content() %>%
     rvest::html_nodes("p") %>%
     rvest::html_text() %>%
--- tests/testthat/test-search.R
+++ tests/testthat/test-search.R
@@ -1,3 +1,4 @@
-test_that("search http req work", {
-  testthat::expect_silent(search(intToUtf8(45432)))
+test_that("query http req work", {
+  expect_message(query(name =intToUtf8(45432)))
+  expect(any(class(query(intToUtf8(45432), msg = F)) %in% "tbl"))
 })
