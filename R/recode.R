afc47f37ec474126223b3e02b9d39fcea3d72450
Chanyub Park <mrchypark@gmail.com>
2019-06-22 14:51:06 +0900

    update readme

---
+++ R/recode.R
@@ -0,0 +1,34 @@
+#' jaesan data detail called recode.
+#'
+#' Property of People who Work(ed) for Korea
+#'
+#' @source https://jaesan.newstapa.org/
+#' @format tibble with columns
+#' @examples
+#'   recode2011
+#'   recode2012
+#'   recode2013
+#'   recode2014
+#'   recode2015
+#'   recode2016
+#'   recode2017
+#'   recode2018
+#' @name recode
+NULL
+
+#' @rdname recode
+"recode2011"
+#' @rdname recode
+"recode2012"
+#' @rdname recode
+"recode2013"
+#' @rdname recode
+"recode2014"
+#' @rdname recode
+"recode2015"
+#' @rdname recode
+"recode2016"
+#' @rdname recode
+"recode2017"
+#' @rdname recode
+"recode2018"
